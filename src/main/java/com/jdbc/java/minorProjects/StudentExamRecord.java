3cf425152bf3e241794c587ed0342a1dd957e033
Dhivyadarsshni <dhivyadarsshni16@gmail.com>
2025-07-12 13:36:46 +0530

    updated StudentExamRecord file with third working method

--- src/main/java/com/jdbc/java/minorProjects/StudentExamRecord.java
+++ src/main/java/com/jdbc/java/minorProjects/StudentExamRecord.java
@@ -1,6 +1,8 @@
 package com.jdbc.java.minorProjects;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentExamRecord {
@@ -90,6 +92,79 @@ public class StudentExamRecord {
     }
     //Update Student function
     private static void updateStudent(Scanner sc){
+        try(Connection conn = Conn_StudentExamRecord.getConnection()){
+            System.out.println("Enter the student ID to update details for: ");
+            String id = sc.next();
+
+            String checkQuery = "SELECT* FROM STUDENT WHERE id =?";
+
+            PreparedStatement checkstmt = conn.prepareStatement(checkQuery);
+            checkstmt.setString(1,id);
+            ResultSet rs = checkstmt.executeQuery();
+
+            if(!rs.next()){ System.out.println("Wrong id entered: "+id); return;}
+
+
+
+            System.out.println("The fields given are\n1.Name\n2.Department\n3.Company\n4.Marks Scored\n5.Total Marks");
+            System.out.println("In total how many fields do you want to update? ");
+            int fieldCount = sc.nextInt();
+            sc.nextLine();
+
+            //first line of the update query
+            StringBuilder sb = new StringBuilder("UPDATE STUDENT SET ");
+            //we introduced <Object> so it can fit both integer and string values
+            List<Object> values = new ArrayList<>();
+            //introduce a loop to save the field names to update from the user
+            for(int i =1; i<=fieldCount; i++){
+                System.out.println("Enter the field name to update:");
+                System.out.println("name,dept,company_name,scored_marks,total_marks ");
+                System.out.println("(one field at a time)");
+                String field = sc.nextLine();
+
+                System.out.println("Enter the new value for "+field+":");
+                String value = sc.nextLine();
+
+                sb.append(field).append(" =?");
+                if(i != fieldCount )sb.append(" ,");
+
+                if(field.equalsIgnoreCase("scored_marks") || field.equalsIgnoreCase("total_marks"))
+                    values.add(Integer.parseInt(value));
+                else
+                    values.add(value);
+
+            }
+            //append the query statement with WHERE clause using primary key
+            sb.append(" WHERE id = ?");
+            values.add(id);
+
+            //convert sb into a string and prepare the sql query
+            PreparedStatement ps = conn.prepareStatement(sb.toString());
+
+            //set params dynamically into ps
+            for (int i = 0; i< values.size();i++){
+                Object val = values.get(i);
+
+                if(val instanceof Integer)
+                    ps.setInt(i+1,(Integer) val);
+                else
+                    ps.setString(i+1,(String) val);
+            }
+
+            int check = ps.executeUpdate();
+            if(check>0)
+                System.out.println("Student record updated");
+            else
+                System.out.println("uh-oh something went wrong");
+
+        } catch (SQLException sqle) {
+        // A SQL exception (syntax error, connectivity issue, etc.)
+        System.out.println("Oops! Something went wrong with the database.");
+        System.out.println("Error: " + sqle.getMessage());
+    } catch (Exception e) {
+        // Any other unexpected exception
+        System.out.println("⚠️ Unexpected error: " + e.getMessage());
+    }
 
     }
     //View Student function
