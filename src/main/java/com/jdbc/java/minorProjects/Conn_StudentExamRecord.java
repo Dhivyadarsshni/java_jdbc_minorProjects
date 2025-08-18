a499b6dfd9ec68ff4a5b3aaa5d5193da0302e4c6
Dhivyadarsshni_dd <85487484+Dhivyadarsshni@users.noreply.github.com>
2025-08-18 17:04:54 +0530

    Updated JDBC

--- src/main/java/com/jdbc/java/minorProjects/Conn_StudentExamRecord.java
+++ src/main/java/com/jdbc/java/minorProjects/Conn_StudentExamRecord.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Conn_StudentExamRecord {
         private static final String url = "jdbc:mysql://localhost:3306/student_db";
         private static final String username = "root";
-        private static final String pass = "Dhivya2003@";
+        private static final String pass = "PASS";
 
         public static Connection getConnection() throws SQLException{
             return DriverManager.getConnection(url,username,pass);
